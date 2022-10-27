c3557ab57a4965846931c48387feb78fe8834f1a
Aan Kristanto Nugroho <aankristanto1@gmail.com>
2022-10-27 16:01:21 +0700

    adding route cutting

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import userRoute from "./setup/user.route.js";
+
 import { Login, Logout } from "../controllers/auth/Login.js";
 import { refreshToken } from "../controllers/auth/RefreshToken.js";
 import userAccesRoute from "./auth/userAccess.route.js";
 import getMenu from "../controllers/setup/Menu.js";
 import { getDept, getDeptById } from "../controllers/setup/Dept.js";
+import cuttingRoute from "./production/cutting.route";
 
 const router = express.Router();
 
@@ -15,11 +17,11 @@ router.get("/menu", getMenu);
 router.get("/dept", getDept);
 router.get("/dept/:id", getDeptById);
 router.use("/useraccess", userAccesRoute);
-
 router.use("/user", userRoute);
+router.use("/cutting", cuttingRoute);
 
 router.all("*", (req, res, next) => {
-  next(new ExpressError("Page Not Found", 400));
+  next(new ExpressError("Page Not Found", 404));
 });
 
 router.use((err, req, res, next) => {
---
+++ routes/production/cutting.route.js
@@ -0,0 +1,14 @@
+import express from "express";
+const router = express.Router();
+
+import { getOrder, getOrderByBarcodeSerial, getOrderByBLK, newOrder, deleteOrder, ScanCutting } from "../../controllers/production/cutting/Cutting";
+
+// ROUTE CUTTING
+router.get("/order", getOrder);
+router.get("/order/:barcodeserial", getOrderByBarcodeSerial);
+router.get("/order/:orderno", getOrderByBLK);
+router.post("/order", newOrder);
+router.delete("/order/:barcodeserial", deleteOrder);
+router.post("/order", ScanCutting);
+
+export default router;
