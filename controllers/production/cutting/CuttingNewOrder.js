0e260ee63e2de1873481f0d5cc75b3d35215b7c6
Aan Kristanto Nugroho <aankristanto1@gmail.com>
2022-10-28 13:35:24 +0700

    fixing scanlog cutting

--- controllers/production/cutting/CuttingNewOrder.js
+++ controllers/production/cutting/CuttingNewOrder.js
@@ -27,12 +27,9 @@ export const newOrder = async (req, res) => {
                 existData.push(...checkBarcodeSerial);
             } else {
                 await Orders.create(order);
-                await ScanCutting.create({
-                    BARCODE_SERIAL: order.BARCODE_SERIAL
-                });
-                
+                    
             }
-
+            
             if (i + 1 === dataOrder.length)
                 return res.status(201).json({
                     success: true,
@@ -40,67 +37,6 @@ export const newOrder = async (req, res) => {
                     duplicate: existData,
                 });
         });
-
-        /*
-        const {
-            buyercode,
-            orderno,
-            producttype,
-            buyerpo,
-            mono,
-            orderversion,
-            shipmentdate,
-            orderqty,
-            ordercolor,
-            ordersize,
-            orderstyle,
-            barcodeserial,
-            siteline
-        } = req.body;
-
-        const checkBarcodeSerial = await Orders.findAll({
-            attributes: ["BARCODE_SERIAL"],
-            where: {
-                BARCODE_SERIAL: barcodeserial,
-            }
-        });
-
-        if (checkBarcodeSerial.length !== 0) {
-            return res.status(400).json({
-                success: false,
-                message: `insert order data for ${orderno}/${buyerpo}/${mono}${barcodeserial} failed, barcode serial exist!`,
-                data: req.body
-            });
-        }
-        
-        await ScanCutting.create({
-            BARCODE_SERIAL: barcodeserial
-        });
-        
-        await Orders.create({
-            BUYER_CODE: buyercode,
-            ORDER_NO: orderno,
-            PRODUCT_TYPE: producttype,
-            BUYER_PO: buyerpo,
-            MO_NO: mono,
-            ORDER_VERSION: orderversion,
-            SHIPMENT_DATE: shipmentdate,
-            ORDER_QTY: orderqty,
-            ORDER_COLOR: ordercolor,
-            ORDER_SIZE: ordersize,
-            ORDER_STYLE: orderstyle,
-            BARCODE_SERIAL: barcodeserial,
-            SITE_LINE: siteline
-        });
-        
-
-        
-        res.status(200).json({
-            success: true,
-            message: `Order Data for ${orderno}/${buyerpo}/${mono}${barcodeserial} Added Successfully`,
-            data: []
-        });
-        */
     } catch (error) {
         res.status(404).json({
             success: false,
