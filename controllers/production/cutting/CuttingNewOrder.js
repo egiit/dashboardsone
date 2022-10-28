eb29d797a16231da50a36b2b8f46e7162cf9a19a
Aan Kristanto Nugroho <aankristanto1@gmail.com>
2022-10-28 13:17:37 +0700

    fixing new order

--- controllers/production/cutting/CuttingNewOrder.js
+++ controllers/production/cutting/CuttingNewOrder.js
@@ -4,6 +4,44 @@ import moment from "moment";
 // CONTROLLER CREATE NEW ORDER DATA
 export const newOrder = async (req, res) => {
     try {
+        let existData = [];
+        const dataOrder = req.body;
+
+        if (!dataOrder.length){
+            return res.status(404).json({
+                success: false,
+                message: "no data upload!",
+                data: dataOrder
+            });
+        }
+
+        dataOrder.forEach(async (order, i) => {
+            const checkBarcodeSerial = await Orders.findAll({
+                attributes: ["BARCODE_SERIAL"],
+                where: {
+                    BARCODE_SERIAL: order.BARCODE_SERIAL,
+                },
+            });
+
+            if (checkBarcodeSerial.length !== 0) {
+                existData.push(...checkBarcodeSerial);
+            } else {
+                await Orders.create(order);
+                await ScanCutting.create({
+                    BARCODE_SERIAL: order.BARCODE_SERIAL
+                });
+                
+            }
+
+            if (i + 1 === dataOrder.length)
+                return res.status(201).json({
+                    success: true,
+                    message: "Order Data Added Successfully",
+                    duplicate: existData,
+                });
+        });
+
+        /*
         const {
             buyercode,
             orderno,
@@ -54,6 +92,7 @@ export const newOrder = async (req, res) => {
             BARCODE_SERIAL: barcodeserial,
             SITE_LINE: siteline
         });
+        
 
         
         res.status(200).json({
@@ -61,6 +100,7 @@ export const newOrder = async (req, res) => {
             message: `Order Data for ${orderno}/${buyerpo}/${mono}${barcodeserial} Added Successfully`,
             data: []
         });
+        */
     } catch (error) {
         res.status(404).json({
             success: false,
