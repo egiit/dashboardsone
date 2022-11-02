870effe50a1802507e0cd52a41b129fa4a7c940c
Aan Kristanto Nugroho <aankristanto1@gmail.com>
2022-11-02 15:31:46 +0700

    remove comment

--- routes/production/cutting.route.js
+++ routes/production/cutting.route.js
@@ -1,10 +1,6 @@
 import express from "express";
 const router = express.Router();
 
-// import { getOrder, getOrderByBarcodeSerial, getOrderByBLK } from "../../controllers/production/cutting/CuttingGetOrder.js";
-// import newOrder from "../../controllers/production/cutting/CuttingNewOrder.js";
-// import deleteOrder from "../../controllers/production/cutting/CuttingDeleteOrder.js";
-
 import QRScanCutting from "../../controllers/production/cutting/CuttingScan.js";
 import {
   getCutingOrder,
