a17ec014dba1fd2f328af674d17d3f681d94a7f5
Aan Kristanto Nugroho <aankristanto1@gmail.com>
2022-11-03 09:28:21 +0700

    adding query for workdone on model

--- models/production/cutting.mod.js
+++ models/production/cutting.mod.js
@@ -28,10 +28,27 @@ export const OrderDetailList = `SELECT a.BUYER_CODE, a.ORDER_NO, a.MO_NO, SUM(a.
 FROM order_detail a GROUP BY a.BUYER_CODE, a.ORDER_NO
 ORDER BY a.DATE_CREATE DESC`;
 
-export const SelectOrderNo = `SELECT DISTINCT a.BUYER_CODE, a.ORDER_NO, a.PRODUCT_TYPE, a.BUYER_PO, a.MO_NO, a.ORDER_VERSION, a.SHIPMENT_DATE,
+export const SelectOrderNo = `SELECT DISTINCT 
+a.BUYER_CODE, a.ORDER_NO, a.PRODUCT_TYPE, a.BUYER_PO, a.MO_NO, a.ORDER_VERSION, a.SHIPMENT_DATE,
 a.ORDER_QTY, a.ORDER_SIZE, a.ORDER_STYLE, a.BARCODE_SERIAL, a.SITE_LINE, b.ITEM_COLOR_NAME, SUBSTRING_INDEX(b.ORDER_REFERENCE_PO_NO, ' ', 1) ORDER_REF, b.COUNTRY
 FROM order_detail a 
 LEFT JOIN (
 	SELECT * FROM order_po_listing c WHERE c.ORDER_NO = :orderNo
 	) b ON b.MO_NO = a.MO_NO
 WHERE a.ORDER_NO = :orderNo ORDER BY  a.ORDER_SIZE, a.BARCODE_SERIAL`;
+
+export const CuttingWorkdoneByDate = `SELECT
+    order_detail.BUYER_CODE AS BuyersCode,
+    order_scan_log.BARCODE_SERIAL AS BarcodeSerial,
+    order_detail.ORDER_NO AS OrderNumber,
+    order_detail.BUYER_PO AS BuyerPO,
+    order_detail.MO_NO AS MONumber,
+    DATE_FORMAT(order_scan_log.CUTTING_SCAN_TIME, '%Y-%m-%d') AS ScanDate,
+    TIME_FORMAT(order_scan_log.CUTTING_SCAN_TIME, "%H:%i:%s") AS ScanTime,
+    order_detail.ORDER_QTY AS Qty,
+    '1' AS StepID,
+    SUBSTRING(order_scan_log.CUTTING_SCAN_LOCATION, 1, 5) AS Site,
+    order_scan_log.CUTTING_SCAN_LOCATION AS Line
+FROM order_scan_log
+INNER JOIN order_detail ON order_scan_log.BARCODE_SERIAL=order_detail.BARCODE_SERIAL
+WHERE DATE(order_scan_log.CUTTING_SCAN_TIME) BETWEEN :startDate AND :endDate ORDER BY order_scan_log.CUTTING_SCAN_TIME`;
--- models/production/packing.mod.js
+++ models/production/packing.mod.js
@@ -20,3 +20,19 @@ export const ScanPacking = db.define(
 );
 
 ScanPacking.removeAttribute("id");
+
+export const PackingWorkdoneByDate = `SELECT
+    order_detail.BUYER_CODE AS BuyersCode,
+    order_scan_log.BARCODE_SERIAL AS BarcodeSerial,
+    order_detail.ORDER_NO AS OrderNumber,
+    order_detail.BUYER_PO AS BuyerPO,
+    order_detail.MO_NO AS MONumber,
+    DATE_FORMAT(order_scan_log.PACKING_SCAN_TIME, '%Y-%m-%d') AS ScanDate,
+    TIME_FORMAT(order_scan_log.PACKING_SCAN_TIME, "%H:%i:%s") AS ScanTime,
+    order_detail.ORDER_QTY AS Qty,
+    '4' AS StepID,
+    SUBSTRING(order_scan_log.PACKING_SCAN_LOCATION, 1, 5) AS Site,
+    order_scan_log.PACKING_SCAN_LOCATION AS Line
+FROM order_scan_log
+INNER JOIN order_detail ON order_scan_log.BARCODE_SERIAL=order_detail.BARCODE_SERIAL
+WHERE DATE(order_scan_log.PACKING_SCAN_TIME) BETWEEN :startDate AND :endDate ORDER BY order_scan_log.PACKING_SCAN_TIME`;
