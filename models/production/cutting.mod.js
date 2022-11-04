e63a3f52c8e3ea05ca48784a2fcbf171f305ea95
Aan Kristanto Nugroho <aankristanto1@gmail.com>
2022-11-04 12:45:50 +0700

    rename workdone view

--- models/production/cutting.mod.js
+++ models/production/cutting.mod.js
@@ -69,19 +69,4 @@ LEFT JOIN (
 	) b ON b.MO_NO = a.MO_NO
 WHERE a.ORDER_NO = :orderNo ORDER BY  a.ORDER_SIZE, a.BARCODE_SERIAL`;
 
-export const CuttingWorkdoneByDate = `SELECT
-    order_detail.BUYER_CODE AS BuyersCode,
-    order_scan_log.BARCODE_SERIAL AS BarcodeSerial,
-    order_detail.ORDER_NO AS OrderNumber,
-    order_detail.BUYER_PO AS BuyerPO,
-    order_detail.MO_NO AS MONumber,
-    DATE_FORMAT(order_scan_log.CUTTING_SCAN_TIME, '%Y-%m-%d') AS ScanDate,
-    TIME_FORMAT(order_scan_log.CUTTING_SCAN_TIME, "%H:%i:%s") AS ScanTime,
-    order_detail.ORDER_QTY AS Qty,
-    '1' AS StepID,
-    SUBSTRING(order_scan_log.CUTTING_SCAN_LOCATION, 1, 5) AS Site,
-    order_scan_log.CUTTING_SCAN_LOCATION AS Line
-FROM order_scan_log
-INNER JOIN order_detail ON order_scan_log.BARCODE_SERIAL=order_detail.BARCODE_SERIAL
-WHERE DATE(order_scan_log.CUTTING_SCAN_TIME) BETWEEN :startDate AND :endDate ORDER BY order_scan_log.CUTTING_SCAN_TIME`;
-
+export const CuttingWorkdoneByDate = `SELECT * FROM ViewWorkdoneCutting WHERE ScanDate BETWEEN :startDate AND :endDate`;
