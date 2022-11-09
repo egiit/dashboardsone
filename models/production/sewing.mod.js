670aa64d91f0415380a2c3247439805b673e6631
Aan Kristanto Nugroho <aankristanto1@gmail.com>
2022-11-09 15:46:53 +0700

    add model sewing siteline

--- models/production/sewing.mod.js
+++ models/production/sewing.mod.js
@@ -18,10 +18,59 @@ export const ScanSewing = db.define(
         freezeTableName: true,
         createdAt: false,
         updatedAt: false,
-      }
+    }
+);
+
+export const SewingSiteLine = db.define(
+    "item_siteline", {
+        ID_SITELINE: {
+            type: DataTypes.STRING(10),
+            allowNull: false
+        },
+        SITE: {
+            type: DataTypes.STRING(50),
+            allowNull: true
+        },
+        LINE: {
+            type: DataTypes.STRING(50),
+            allowNull: true
+        },
+        SHIFT: {
+            type: DataTypes.INTEGER(11),
+            allowNull: true
+        },
+        START_TIME: {
+            type: DataTypes.TIME,
+            allowNull: true
+        },
+        END_TIME: {
+            type: DataTypes.TIME,
+            allowNull: true
+        },
+        CREATE_DATE: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+        CREATE_BY: {
+            type: DataTypes.INTEGER(11),
+            allowNull: true
+        },
+        UPDATE_DATE: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+        UPDATE_BY: {
+            type: DataTypes.INTEGER(11),
+            allowNull: true
+        }
+    }, {
+        freezeTableName: true,
+        createdAt: false,
+        updatedAt: false,
+    }
 );
 
 ScanSewing.removeAttribute("id");
-
+SewingSiteLine.removeAttribute("id");
 
 export const SewingWorkdoneByDate = `SELECT * FROM ViewWorkdoneSewing WHERE ScanDate BETWEEN :startDate AND :endDate`;
