370d1e060f4bab3de15d34c3d75d7240872b4c5c
egiit <giefilth@gmail.com>
2022-12-08 13:44:01 +0700

    edit viewcapacity

--- config/database.js
+++ config/database.js
@@ -5,17 +5,7 @@ import { Sequelize } from "sequelize";
 const { DB_NAME, HOST, USER, PASS } = process.env;
 tedious.Connection;
 
-const db = new Sequelize("db_sumbiri_one", "root", "", {
-  dialect: "mysql",
-  logging: false,
-  timezone: "+07:00",
-  dialectOptions: {
-    timezone: "local",
-  },
-});
-
-// const db = new Sequelize("db_sumbiri_one", "egi", "Asd54321`", {
-//   host: "192.168.2.82",
+// const db = new Sequelize("db_sumbiri_one", "root", "", {
 //   dialect: "mysql",
 //   logging: false,
 //   timezone: "+07:00",
@@ -23,5 +13,15 @@ const db = new Sequelize("db_sumbiri_one", "root", "", {
 //     timezone: "local",
 //   },
 // });
+
+const db = new Sequelize("db_sumbiri_one", "egi", "Asd54321`", {
+  host: "192.168.2.82",
+  dialect: "mysql",
+  logging: false,
+  timezone: "+07:00",
+  dialectOptions: {
+    timezone: "local",
+  },
+});
 
 export default db;
