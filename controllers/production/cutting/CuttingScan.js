b6d663b8bdd150eb3440dc5bcf9f4235a0209bf6
egiit <giefilth@gmail.com>
2023-01-13 10:29:14 +0700

    updat new scan sewing in

--- controllers/production/cutting/CuttingScan.js
+++ controllers/production/cutting/CuttingScan.js
@@ -11,6 +11,7 @@ import {
   QueryCheckSchdScan,
   QueryfindQrSewingIn,
 } from "../../../models/planning/dailyPlan.mod.js";
+import { GetQrlistAftrScan } from "../../../models/production/sewing.mod.js";
 
 // CONTROLLER SCAN CUTTING
 export const QRScanCutting = async (req, res) => {
@@ -113,16 +114,6 @@ export const QRScanSewingIn = async (req, res) => {
         });
       }
 
-      const paramsFind = {
-        plannDate: schDate,
-        sitename: sitename,
-        lineName: lineName ? lineName : valueBarcode.LINE_NAME,
-        moNo: valueBarcode.MO_NO,
-        styleDesc: valueBarcode.ORDER_STYLE,
-        colorCode: valueBarcode.ORDER_COLOR,
-        sizeCode: valueBarcode.ORDER_SIZE,
-      };
-
       //find schedule
       const checkSchdNsize = await db.query(QueryCheckSchdScan, {
         replacements: {
@@ -146,19 +137,89 @@ export const QRScanSewingIn = async (req, res) => {
           SEWING_SCAN_BY: userId,
           SEWING_SCAN_LOCATION: sitename,
         };
+        const returnData = {
+          ...valueBarcode,
+          LINE_NAME: lineName ? lineName : valueBarcode.LINE_NAME,
+          SCHD_ID,
+          SCH_ID,
+          SITE_NAME: sitename,
+        };
         const pushQrSewin = await CuttinScanSewingIn.create(dataBarcode);
         if (pushQrSewin)
           return res.status(200).json({
             success: true,
             qrstatus: "success",
             message: "Scan Success",
+            data: returnData,
           });
       }
 
       return res.status(200).json({
         success: true,
         qrstatus: "error",
-        message: "Pls Find Line",
+        message: "No Schedule",
+      });
+    }
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      data: error,
+      message: "error processing request",
+    });
+  }
+};
+
+export const QrListAftrSewingIn = async (req, res) => {
+  try {
+    const { schDate, sitename, barcodeserial } = req.params;
+
+    const listQrAfterScan = await db.query(GetQrlistAftrScan, {
+      replacements: { schDate, sitename, barcodeserial },
+      type: QueryTypes.SELECT,
+    });
+
+    if (listQrAfterScan)
+      return res.status(200).json({
+        success: true,
+        message: "Found Data Scan",
+        data: listQrAfterScan,
+      });
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      data: error,
+      message: "error processing request",
+    });
+  }
+};
+
+//delete qr sewing IN
+export const DelQrScanSewIN = async (req, res) => {
+  try {
+    const { barcodeserial } = req.params;
+
+    const checkQr = await CuttinScanSewingIn.findAll({
+      where: {
+        BARCODE_SERIAL: barcodeserial,
+      },
+    });
+
+    if (!checkQr)
+      return res.status(200).json({
+        success: false,
+        message: "QR Not Found",
+      });
+
+    const deleteQr = await CuttinScanSewingIn.destroy({
+      where: {
+        BARCODE_SERIAL: barcodeserial,
+      },
+    });
+
+    if (deleteQr) {
+      return res.status(200).json({
+        success: true,
+        message: "QR Deleted",
       });
     }
   } catch (error) {
--- routes/production/cutting.route.js
+++ routes/production/cutting.route.js
@@ -2,6 +2,8 @@ import express from "express";
 const router = express.Router();
 
 import {
+  DelQrScanSewIN,
+  QrListAftrSewingIn,
   QRScanCutting,
   QRScanSewingIn,
 } from "../../controllers/production/cutting/CuttingScan.js";
@@ -15,9 +17,14 @@ import {
 
 router.get("/order/list/:startDate/:endDate", getCuttingOrder);
 router.get("/order/bundle/:orderNo", getOrderByBLK);
+router.get(
+  "/qr/scan-sewing-in/:schDate/:sitename/:barcodeserial",
+  QrListAftrSewingIn
+);
 
 router.post("/order/qrgenerate", newQRCutting);
 router.post("/order/scan", QRScanCutting);
 router.post("/qr/scan-sewing-in", QRScanSewingIn);
 
+router.delete("/qr/scan-sewing-in/:barcodeserial", DelQrScanSewIN);
 export default router;
